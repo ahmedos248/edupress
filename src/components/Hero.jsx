72c000865203208ce48332d0dc99d36e6cc42ea0
Ahmed <hikalahmed10@gmail.com>
2025-08-14 00:51:26 +0300

    fix hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section className="bg-white w-full pt-24 px-10 pb-24 lg:py-44 lg:px-40 relative bg-[url('./public/images/hero.jpg')] bg-cover lg:bg-center md:bg-right bg-left">
+        <section className="bg-white w-full pt-24 px-10 pb-24 lg:py-44 lg:px-40 relative bg-[url('./images/hero.jpg')] bg-cover lg:bg-center md:bg-right bg-left">
             <div className="grid lg:grid-cols-2 gap-10 items-center">
                 <div className="space-y-6 z-10 ">
                     <h1 className="text-4xl lg:w-96 w-72 lg:text-5xl font-extrabold leading-tight text-gray-900 exo-text">
